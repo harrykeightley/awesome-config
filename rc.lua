176e5d0f152221ea74cc9a6b268cc59ba23646af
Harry Keightley <harrykeightley@outlook.com>
2024-04-02 22:07:14 +1000

    Remove old lua from entrypoint

--- rc.lua
+++ rc.lua
@@ -2,31 +2,6 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
--- -- Standard awesome library
--- local gears = require("gears")
-local awful = require("awful")
---
--- RC = {}
-
--- -- a variable needed in statusbar (helper)
--- RC.launcher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = RC.mainmenu })
---
--- -- Menubar configuration
--- -- Set the terminal for applications that require it
--- menubar.utils.terminal = RC.vars.terminal
---
---
-mykeyboardlayout = awful.widget.keyboardlayout()
---
--- Basic numbers along the bottom
--- require("deco.statusbar")
---
---
--- require("main.signals")
--- require("deco.styling")
---
--- require("main.post-config")
---
 -- FENNEL TIME BABY
 local fennel = require("./fennel").install()
 fennel.path = fennel.path .. ";.config/awesome/?.fnl"
